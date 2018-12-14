9c0a1042357c87c5d546a226ba5b7c9fe05f40d8
李金起 <jinqili@hniot.com>
2018-12-14 14:17:36 +0800

    修改示例

--- src/SuperRabbitMQClient/RabbitMQClient/ExampleClient1.cs
+++ src/SuperRabbitMQClient/RabbitMQClient/ExampleClient1.cs
@@ -9,7 +9,7 @@ namespace SuperRabbitMQClient.RabbitMQClient
 {
     public class ExampleClient1 : RabbitMQClientBase<Command.ExampleClient1.AbstractCommand>
     {
-        public override string IPAddress => zz;
+        public override string IPAddress => base.IPAddress;
         public override string VirtualHost => base.VirtualHost;
         public override string UserName => base.UserName;
         public override string PassWd => base.PassWd;
