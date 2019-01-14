e2ae074c3bdbacc7869ba4d6e7016c72a7194b8d
StrangerLi <lijinqi2008@gmail.com>
2019-01-14 20:56:46 +0800

    修改项目属性文件，并添加Nuget发布文件

--- src/SuperRabbitMQClient/Properties/AssemblyInfo.cs
+++ src/SuperRabbitMQClient/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("SuperRabbitMQClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A lightweight client for connecting to RabbitMQ")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("StrangerLi")]
 [assembly: AssemblyProduct("SuperRabbitMQClient")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 StrangerLi")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
